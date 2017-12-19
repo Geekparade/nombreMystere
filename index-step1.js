c223f1a0aebf6cf0b78dacf794726b3e9040eb86
Geekparade <david_da_silva0hotmail.fr>
2017-12-19 12:26:19 +0100

    exo 1 finish

--- index-step1.js
+++ index-step1.js
@@ -5,23 +5,28 @@ function main(){
 
 	// == Fonction clickValider == 
 	// Créer la fonction 'clickValider'
-
+	function clickValider(){
+		var number= $("#value").val() ;
+		var chiffre=21 ;
 		// Récupérer le contenu de mon input
-
 		// Comparer ce contenu à un nombre que vous choisissez
 			// Si le contenu est égal
 			// Afficher une alert Gagné
-
+			if(number == chiffre){
+				alert("You Win");
+			}
 			// Sinon,
 			// Afficher une alert Perdu
-
-
+			else{
+				alert("You Lose");
+			}
 	// /Fin == Fonction clickValider == 
-
-
-
+		}
 	// Ecrire le code qui va détecter que le bouton 'valider' est cliqué.
 	// Et qui appelle la fonction 'clickValider' défini ci-dessus
-
-
+	$("#btn").click(function(){
+		clickValider();
+			
+		
+	})
 }
